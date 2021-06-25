64b235c3725619d49e1ce7a506b24f9949af533c
Gaurav Sharma <sharmag8121@gmail.com>
2021-06-25 05:16:44 -0700

    pointers

--- pointers/basic.cpp
+++ pointers/basic.cpp
@@ -2,5 +2,11 @@
 using namespace std;
 
 int main(){
-
+    // &return memory address of its operand
+    // *returns the value of the variable located at the address following it
+    int val = 25;
+    int *ptr = &val;
+    cout<<ptr<<"\n";
+    cout<<*ptr;
+    return 0;
 }
