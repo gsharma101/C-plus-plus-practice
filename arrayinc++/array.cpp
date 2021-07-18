8c5bb4f7cb5975a43b05a978e0c733ea5ba37a98
Gaurav Sharma <sharmag8121@gmail.com>
2021-07-18 08:57:42 +0530

    Array in c++ first commit

---
+++ arrayinc++/array.cpp
@@ -0,0 +1,7 @@
+#include <bits\stdc++.h>
+//! Program re read marks of 50 students and store them under an array (for demo i have taken 5 student)
+using namespace std;
+
+int main(){
+    return 0;
+}
--- pointers/arrays__pointers.cpp
+++ pointers/arrays__pointers.cpp
@@ -5,5 +5,10 @@ int main(){
     int a[] = {6,2,1,5,3};
     cout<<a<<"\n";
     cout<<&a<<"\n";
+    // a = &a this is only possible in arrays 
+    cout<<a[2]<<"\n";
+    cout<< *(a + 2)<<"\n";
+    cout<< a[3]<<"\n";
+    cout<< 3[a]<<"\n";
     return 0;
 }
