41d6cafb98cb9a79ecb19476eb8f6adc354143c6
Gaurav Sharma <sharmag8121@gmail.com>
2021-07-20 23:33:28 +0530

    updating array

--- arrayinc++/array.cpp
+++ arrayinc++/array.cpp
@@ -3,5 +3,15 @@
 using namespace std;
 
 int main(){
+    const int size = 5;
+    float marks[size];
+    for(int i=0;i<size;i++){ //?Looping through 5 elements
+        cout<<"Enter marks of students "<<i+1<<"\n";
+        cin>>marks[i];
+    }
+    cout<<"\n";
+    for(int i = 0; i<size; i++){
+        cout<<"Marks["<<i<<"]="<<marks[i]<<"\n";
+    }
     return 0;
 }
