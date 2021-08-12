42a1d35ee5fed4c60375c719c0b0fa4b10b0fd25
Gaurav Sharma <sharmag8121@gmail.com>
2021-08-12 23:40:19 +0530

    Updating flow of control

--- flow of control/areausingswitch.cpp
+++ flow of control/areausingswitch.cpp
@@ -2,18 +2,16 @@
 using namespace std;
 
 int main(){
-   float area,rad,len,bre,a,b,c,s;
+   float area,rad,len,a,b,c,s;
    int ch;
    cout<<"Area Menu\n";
    cout<<"1.Circle\n";
    cout<<"2.Rectangle\n";
    cout<<"3.Triangle\n";
-   cout<<"\nEnter your choice:";
    cin>>ch;
    cout<<"\n";
-   switch(ch)
-   {
-       case 1: cout<<"Enter the radius of the circle"
+   switch(ch){
+
    }
    return 0;
 }
