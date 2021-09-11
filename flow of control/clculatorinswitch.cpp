3af6e262bd11972968c020905cd05011cc2d5c44
Gaurav Sharma <sharmag8121@gmail.com>
2021-09-11 23:19:09 +0530

    calculator using switch

---
+++ flow of control/clculatorinswitch.cpp
@@ -0,0 +1,39 @@
+#include <bits\stdc++.h>
+using namespace std;
+
+int main(){
+    float op1,op2,res;
+    char ch;
+    cout<<"Enter two numbers \n";
+    cin>>op1>>op2;
+    cout<<"\n"<<"Enter any operator(+,-,*,/,%):";
+    cin>>ch;
+    cout<<"\n";
+    switch(ch)
+    {
+        case '+': res = op1 + op2;
+                break;
+        case '-': res = op1 - op2;
+                break;
+        case '/': if(op2 == 0)
+                    cout<<"Divide by zero is error!!";
+                else
+                    res = op1 / op2;
+                break;
+        case '*':   res = op1 * op2;
+                break;
+        case '%': if(op2 == 0)
+                cout<<"Divide by zero error!!";
+            else
+                {
+                    int r,q;
+                    q = op2/op1;
+                    r = op2 - (q * op1);
+                    res = r;
+                }
+                break;
+        default: cout<<"\n"<<"Wrong operator"<<"\n";
+    }
+    cout<<"The calculation resulr is:"<<res<<"\n";
+    return 0;
+}
