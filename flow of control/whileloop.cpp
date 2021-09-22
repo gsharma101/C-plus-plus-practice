7d25b84686316bdf81f5f719f91243749361bc65
Gaurav Sharma <sharmag8121@gmail.com>
2021-09-22 17:28:33 +0530

    Updating flow of control for looping statements

--- flow of control/whileloop.cpp
+++ flow of control/whileloop.cpp
@@ -1,6 +1,14 @@
 #include <bits\stdc++.h>
 using namespace std;
-
+// To claculate the factorial of any number
 int main(){
-  
+  int i,num,fact=1;
+  cout<<"Enter any number\n";
+  cin>>num;
+  i = num;
+  while(num){
+      fact *= num;
+      --num;
+  }
+  cout<<"The factorial of "<<i<<" is "<<fact;
 }
