d7eeda7b29203336add76e873e5239eea29f810d
gsharma101 <sharmag8121@gmail.com>
2022-02-05 00:56:43 +0530

    Re-structuring of files

---
+++ 02 - functions in cpp/introductiontofunction.cpp
@@ -0,0 +1,15 @@
+#include <bits\stdc++.h>
+using namespace std;
+
+int add(int a,int b){
+    cout<< a + b;
+}
+
+int main(){
+    int i,j;
+    cout<<"Enter two Numbers"<<"\n";
+    cin>>i>>j;
+
+    add(i,j);
+    return 0;
+}
